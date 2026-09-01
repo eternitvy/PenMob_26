acbf60309334c5a88605db8a145f64006827c451
Regita Abelia <crrissant@gmail.com>
2026-09-01 13:55:46 +0700

    Video 4 Modifikasi 1

---
+++ Minggu_1/Video4/bin/video4.dart
@@ -0,0 +1,8 @@
+import 'package:video4/video4.dart' as video4;
+const String name = "Regita";
+const int age = 20; 
+
+void main(List<String> arguments) {
+  // print('Hello world: ${video4.calculate()}!');
+  print('Nama saya $name, umur saya $age tahun');
+}
